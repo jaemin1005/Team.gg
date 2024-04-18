f4fd19c1b5234be601ec701c88702be86c06d2e3
jaemin1005 <ghkdwoals1005@gmail.com>
2024-04-18 23:11:12 +0900

    TEST Server

--- Clientest.js
+++ Clientest.js
@@ -1,5 +1,6 @@
 var http = require('http');
 const path = require('path');
+
 var clientOption = {
   host: "localhost",
   port: "3000",
@@ -15,20 +16,44 @@ var clientOption = {
 //   res.on('end', () => console.log("Client : Res End"));
 // }
 
-function readJSONResponse(response) {
+function readPOSTJSONResponse(response) {
   var responseData = '';
-  response.on('data', function (chunk) {
-    responseData += chunk;
-  });
-  response.on('end', function () {
-    var dataObj = JSON.parse(responseData);
-    console.log("Raw Response: " +responseData);
-    console.log("Message: " + dataObj.message);
-    console.log("Question: " + dataObj.question);
+
+  response.on('data', (data) => responseData += data);
+
+  response.on('end', () => {
+      let obj = JSON.parse(responseData);
+      console.log("User Id : " + obj.puuId);
+      console.log("USer Name : " + obj.gameName);
+      console.log("User Tag : " + obj.tag);      
   });
 }
 
+function readGETResponse(response)
+{
+  let responseData = ""
+  response.on("data", (chunk) => responseData += chunk);
+  response.on("end", () => {
+   console.log("Response Status : ", response.statusCode)
+   console.log("Response Headers : ", response.headers);
+   console.log(responseData); 
+  })
+}
 
-var req = http.request(clientOption, readJSONResponse);
-req.write('{"name":"Bilbo", "occupation":"Burglar"}');
-req.end();
+
+function TestPOSTRequest()
+{
+  let req = http.request(clientOption, readPOSTJSONResponse);
+  req.write('{"command":"reqUser", "detail":"터검니#000"}');
+  req.end();
+}
+
+function TestGETRequest()
+{
+  clientOption.method = "GET";
+  var req = http.request(clientOption, readGETResponse);
+
+  req.end();
+}
+
+TestGETRequest();
--- Server.js
+++ Server.js
@@ -1,3 +1,6 @@
+//* 모듈 분기
+//* 모듈 처리 
+
 /**
  * * 날짜 : 2024.04.17
  * * 이름 : 황재민
@@ -5,6 +8,8 @@
  */
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
+const user = require("./Module/User.js");
 
 const app = http.createServer((req, res) => 
 {
@@ -12,31 +17,43 @@ const app = http.createServer((req, res) =>
    * * statusCode : HTTP 응답 상태 코드는 특정 HTTP 요청이 성공적으로 완료되었는지 알려줌
    * * https://developer.mozilla.org/ko/docs/Web/HTTP/Status
    */
-  // req.on('data', () => ResData(req,res));
 
-
-  // res.writeHead(200); 
-  // res.on('data', () => {ResData(req,res)})
-  // res.end(fs.readFileSync("./ServerTest.html"))
-  var jsonData = "";
-  req.on('data', function (chunk) {
-    jsonData += chunk;
-  });
-  req.on('end', function () {
-    var reqObj = JSON.parse(jsonData);
-    var resObj = {
-      message: "Hello " + reqObj.name,
-      question: "Are you a good " + reqObj.occupation + "?"
-    };
+  if(req.method == "connetction")
+  {
     res.writeHead(200);
-    res.end(JSON.stringify(resObj));
-  });
+    res.write(fs.readFileSync("ServerTest.html"));
+    res.end();  
+  }
 
-}).listen(3000, () => console.log("Server Start!!"));;
+
+  if(req.method == "GET")
+  {
+    //res.setHeader("Content-Type", "text/html");
+    //res.writeHead(200);
+    //res.write(fs.readFileSync("ServerTest.html"));
+    //res.write();
+    //res.end();   
+  }
+
+  if(req.method == "POST")
+  {
+    let jsonData = "";
+    req.on('data',  (data) => jsonData += data );
+
+    req.on('end', function () {
+      let reqObj = JSON.parse(jsonData);
+
+      /**
+      * * API를 이용해 해당 유저의 정보를 받아오는게 아직 구현 X
+      * * CommandAPI(reqObj.command, reqObj.detail) 이런 식으로 구분해서 처리하면 되지 않을까?
+      * * 현재는 정보를 받아왔다고 임의로 작성 하겠음 :)
+      */
+      let dummyObj = new user.CreateUser("#45622", "터검니", "000");
+      res.writeHead(200);
+      res.end(JSON.stringify(dummyObj));
+    });
+  }
+
+}).listen(3000, () => console.log("Server Start!!"));
 
 
-function ResData(req, res)
-{
-  console.log(res);
-  console.log(req);
-}
