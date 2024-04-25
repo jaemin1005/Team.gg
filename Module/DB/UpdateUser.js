205836a1037fca9dd94e449d921fe76789a27ef2
naviadev <qo2oq1020@gmail.com>
2024-04-25 23:05:35 +0900

    UpdateUser.js의 해설 주석 추가

--- Module/DB/UpdateUser.js
+++ Module/DB/UpdateUser.js
@@ -1,3 +1,13 @@
+/*
+ * 날짜 : 2024-04-24
+ * 작성자 : 배성빈
+ * 설명 : Database.js를 리팩토링하여 모듈로 분할
+ * Summoners table의 특정 열을 업데이트, 갱신 
+ ? @ member : summoner db에 접근. 
+ ? @ param {*}: 해당 User의 puuid와 객체화된 데이터를 매개변수로 받음.
+ ? @ return : -
+ * - 범용성에 대해 고려. 해당 모듈로 모든 정보를 업데이트할 수 있나 검토. 
+*/
 const Database = require("better-sqlite3");
 
 class SummonersUpdate{
