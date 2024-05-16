2318dbf9eae042139c85b4452cd414aebdd10511
naviadev <qo2oq1020@gmail.com>
2024-05-16 17:38:55 +0900

    전적 출력 레이아웃 구현, RecordManager 모듈화 -> RecordManager 수정 (tagEnum 수정 필요)

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import { checkUser } from "./Modules/checkUser.js";
 import { RequestUserData, RequestJSONData } from "./Modules/ReqData.js";
-
+import { tagEnum, RecordManager }from "./Modules/userInfo.js"
 let url = "http://localhost:3000"
 
 const reqSummonersUrl = "/summoner/" 
@@ -10,33 +10,78 @@ const reqChamionsUrl = "/champions/"
 //const main = [...document.getElementById("main")].forEach()
 
 const main = TransDOMArrIntoObj(document.getElementById("main").children);
+
 const $search = document.getElementById("search");
+
+let matchData;
+
 
 ClearViewInMain();
 RequestJSONData();
 
-$search.onkeydown = (e) => {
+
+
+let a = function(){
+  OnViewInMain("match")
+  
+  // let rec = new RecordManager(null, matchData[0])
+  
+  // let key = Object.keys(tagEnum)
+  // console.log(key)
+  // for(let i = 0; i < key.length; i++){
+  //   rec.createElement(key[i], tagEnum[key[i]][0])
+  // }
+  
+  // for (let j = 0; j < key.length; j++) {
+    
+  //   let childTag = tagEnum[key[j]][1];
+
+  //   if (childTag === undefined) {
+  //     continue;
+  //   }
+
+  //   rec.appendTag(key[j], childTag["child"]);
+  // }
+  // rec.printPlayer();
+}
+
+
+
+
+
+$search.onkeydown = async (e) => {
+  
   if(e.keyCode == "13"){  
     let searchValue = checkUser($search.value);
     if(searchValue !== undefined){
-      SearchUser(searchValue);
+      await SearchUser(searchValue);
     }
+    a()
   }
+  
 }
+
 
 async function SearchUser(searchValue){
   OnViewInMain("loading");
   let data = searchValue.replace("#", "-");
   data = encodeURI(data);
   let userData = await RequestUserData(url+reqSummonersUrl+data);
-  console.log(userData);
+  matchData = userData.matchInfo
   OnViewInMain("stat");
 }
 
 function OnViewInMain(name){
   Object.keys(main).forEach(key => {
     if(key === name){
-      main[name].style.display = "flex";
+      if(name == "match"){
+        main[name].style.display = "grid";
+      }
+      else{
+        main[name].style.display = "flex";
+      }
+      
+      
     }
     else{
       main[key].style.display = "none";
@@ -57,3 +102,4 @@ function TransDOMArrIntoObj(arrElem){
   }
   return obj;
 }
+
