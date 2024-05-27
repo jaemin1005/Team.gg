e136d1804c4ed49ffabeb57db145d12cb5629539
naviadev <qo2oq1020@gmail.com>
2024-05-27 13:58:35 +0900

    불필요한 코드 (getLength) 수정

--- client/DomModules/PrintUserInfoSection.js
+++ client/DomModules/PrintUserInfoSection.js
@@ -21,8 +21,7 @@ import { PrintManager } from "./PrintManager.js"
 export class PrintUserInfoSection extends PrintManager {
 
   constructor(parentObj, participant, obj, number) {
-    super(parentObj, participant, null)
-    this.nodeLength = super.getLength()
+    super(parentObj)
     this.participant = participant
     this.gameInfoObject = obj
     this.matchNumber = number
@@ -37,18 +36,19 @@ export class PrintUserInfoSection extends PrintManager {
     this.changeCssLayout()
   }
 
-  async printChampionImg() {
+  printChampionImg() {
     let imgTag = document.getElementsByClassName(`UserChampionDiv ${this.matchNumber}`)[0].children[0]
     super.inputContent(imgTag, { url: this.gameInfoObject.champions[this.participant.championId].imgSrc, width: 64, height: 64 })
   }
-  async printKda() {
+
+  printKda() {
     let kda1 = this.participant.kills + "/" + this.participant.deaths + "/" + this.participant.assists
     let kda2 = (this.participant.kills + this.participant.assists) / this.participant.deaths
     super.inputContent(document.getElementsByClassName(`UserKda ${this.matchNumber}`)[0].children[0], kda1)
     super.inputContent(document.getElementsByClassName(`UserKda ${this.matchNumber}`)[0].children[1], kda2.toFixed(2) + " KDA")
   }
 
-  async printRuneImg() {
+  printRuneImg() {
     let mainRuneStyle = this.participant.perks.styles[0].style
     let mainRune = this.participant.perks.styles[0].selections[0].perk
     let subRuneStyle = this.participant.perks.styles[1].style
@@ -64,9 +64,8 @@ export class PrintUserInfoSection extends PrintManager {
       }
     }
   }
-  async printSpellImg() {
+  printSpellImg() {
     let spellDiv = document.getElementsByClassName(`UserSpellDiv ${this.matchNumber}`)[0]
-
 
     for (let i = 0; i < spellDiv.children.length; i++) {
       let spell = this.participant[`summoner${i + 1}Id`]
@@ -75,34 +74,31 @@ export class PrintUserInfoSection extends PrintManager {
   }
 
 
-  async printItemImg() {
+  printItemImg() {
     let div = document.getElementsByClassName(`ItemDiv ${this.matchNumber}`)[0]
     console.log(this.gameInfoObject)
     for (let i = 0; i < 6; i++) {
       let item = this.participant[`item${i}`]
+      if(item == 0){
+        continue
+      }
       super.inputContent(div.children[i], { url: this.gameInfoObject.items[item].imgSrc, width: 32, height: 32 })
     }
   }
 
-  async changeCssLayout() {
+  changeCssLayout() {
     let div = document.getElementsByClassName(`ResultSection ${this.matchNumber}`)[0]
     div.children[0].innerHTML === "승리" ?
       (() => {
         document.getElementsByClassName(`LogContainer ${this.matchNumber}`)[0].style.borderLeft = "10px solid rgba(79, 79, 179, 0.5)";
         document.getElementsByClassName(`LogContainer ${this.matchNumber}`)[0].style.backgroundColor = "rgba(33,58,150,0.15)";
-        
-        // document.getElementsByClassName(`LogContainer ${this.matchNumber}`)[0].style.borderTopRightRadius = "10px";
-        // document.getElementsByClassName(`LogContainer ${this.matchNumber}`)[0].style.borderBottomRightRadius = "10px";
-
-      })() :
-      (() => {
+        div.children[0].style.color = "rgb(33,58,150)"
+      })() : (() => {
         document.getElementsByClassName(`LogContainer ${this.matchNumber}`)[0].style.borderLeft = "10px solid rgba(235, 135, 135, 0.5)";
         document.getElementsByClassName(`LogContainer ${this.matchNumber}`)[0].style.backgroundColor = "rgba(227,70,70,0.15)"
         div.children[0].style.color = "rgb(227,70,70)"
-        // document.getElementsByClassName(`LogContainer ${this.matchNumber}`)[0].style.borderTopRightRadius = "10px";
-        // document.getElementsByClassName(`LogContainer ${this.matchNumber}`)[0].style.borderBottomRightRadius = "10px";
       })()
 
-    div.style.borde
+
   }
 }
